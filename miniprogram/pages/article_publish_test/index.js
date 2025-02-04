5142a12df8653113e69c36af4a6c1ff18f041212
Geeker LStar <ltx1102@outlook.com>
2025-02-04 17:06:29 +0800

    database read and insert (news--article), test version

--- miniprogram/pages/article_publish_test/index.js
+++ miniprogram/pages/article_publish_test/index.js
@@ -1,3 +1,5 @@
+
+
 Page({
   /**
    * 页面的初始数据
@@ -12,10 +14,51 @@ Page({
     console.log(e.detail.data)
     const data = e.detail.data[0];
     if (data && data.content) {
-      console.log(data.content);  // 如果 content 存在，则打印
+      const content = data.content;
+      console.log(content);  // 如果 content 存在，则打印
+      
+      // 写入数据库
+      // 获取数据库实例
+const db = wx.cloud.database();
+
+// 获取集合（表）
+const collection = db.collection('test');
+
+// 查询集合中最大 ID
+collection.orderBy('id', 'desc').limit(1).get({
+  success(res) {
+    let nextId = 1;  // 默认情况下，如果没有数据，ID 应该从 1 开始
+
+    if (res.data.length > 0) {
+      // 如果有数据，获取最大 ID，递增 1
+      nextId = res.data[0].id + 1;
+    }
+
+    // 插入新数据
+    collection.add({
+      data: {
+        id: nextId, // 使用计算出来的递增 ID
+        content: content,
+        is_show: 1
+      },
+      success(insertRes) {
+        console.log('数据插入成功', insertRes);
+      },
+      fail(err) {
+        console.error('数据插入失败', err);
+      }
+    });
+  },
+  fail(err) {
+    console.error('查询最大 ID 失败', err);
+  }
+});
+
+
     } else {
       console.log('没有 content 字段');
     }
   }
   
+
   },)
---
+++ miniprogram/pages/article_show_test/index.js
@@ -0,0 +1,108 @@
+const app = getApp();
+Page({
+  data: {
+    article: {}
+  },
+  onLoad: function (options) {
+
+//     app.getText('https://www.vvadd.com/wxml_demo/demo.txt?v=2',res => {
+//       let obj = app.towxml(res.data,'markdown',{
+        
+//         theme:'light', //主题 dark 黑色，light白色，不填默认是light
+//         base:"www.xxx.com",
+//         events:{      //为元素绑定的事件方法
+//           tap:e => {
+//             console.log('tap',e);
+//           },
+//           change:e => {
+//             console.log('todo',e);
+//           }
+//         }
+//       });
+//       console.log('获取的内容:', res.data);  // 打印获取的内容
+// console.log('转换后的数据:', obj);    // 打印解析后的数据
+
+      
+//       //更新解析数据
+//       this.setData({
+//         article:obj,
+//       });
+//     });
+
+
+//     // 获取数据库实例
+//     const db = wx.cloud.database();
+
+//     // 获取集合（表）
+//     const collection = db.collection('test');
+
+//     // 查询 id 为 1 的数据
+//     collection.where({
+//       id: 1  // 过滤条件，查找 id 为 1 的数据
+//     }).get({
+//       success(res) {
+//         // 如果查询成功，返回数据
+//         console.log('查询成功:', res.data);
+//         const content = res.data[0].content;
+//         console.log(content);
+
+//         // // 将 content 转换成所需的格式
+//         // let result = app.towxml('# hi', 'markdown', {
+//         //   base: 'https://xxx.com',  // 相对资源的base路径
+//         //   theme: 'light',            // 主题，默认`light`
+//         //   events: {                 // 为元素绑定的事件方法
+//         //     tap: (e) => {
+//         //       console.log('tap', e);
+//         //     }
+//         //   }
+//         // });
+
+        
+//       },
+//       fail(err) {
+//         // 如果查询失败，输出错误信息
+//         console.error('查询失败:', err);
+//       }
+//     });
+
+// 确保在回调函数中使用箭头函数，以确保 `this` 上下文正确
+const db = wx.cloud.database();
+const collection = db.collection('test');
+
+collection.where({
+  id: 1
+}).get({
+  success: (res) => {  // 使用箭头函数，确保 this 指向页面实例
+    console.log('查询成功:', res.data);
+    
+    // 获取 content 数据
+    const content = res.data[0].content;
+    console.log(content);
+    
+    // app.getText('https://www.vvadd.com/wxml_demo/demo.txt?v=2', (res) => {
+      let obj = app.towxml(content, 'html', {
+        theme: 'light', // 主题 dark 黑色，light白色，不填默认是light
+        base: "www.xxx.com",
+        events: {
+          tap: e => {
+            console.log('tap', e);
+          },
+          change: e => {
+            console.log('todo', e);
+          }
+        }
+      });
+
+      // 更新解析数据
+      this.setData({
+        article: obj,
+      });
+    // });
+  },
+  fail: (err) => {
+    console.error('查询失败:', err);
+  }
+});
+
+  }
+});
