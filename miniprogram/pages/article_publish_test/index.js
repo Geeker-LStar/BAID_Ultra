5875a605320df58e42f0841cc03b40eddbf0b2af
Geeker LStar <ltx1102@outlook.com>
2025-02-04 14:59:26 +0800

    fixed known bugs of data transition between web-view and miniprogram page

--- miniprogram/pages/article_publish_test/index.js
+++ miniprogram/pages/article_publish_test/index.js
@@ -9,6 +9,13 @@ Page({
    * 获取请求数据
    */
   handleGetMessage: function (e) {
-    console.log(e.detail.data);
+    console.log(e.detail.data)
+    const data = e.detail.data[0];
+    if (data && data.content) {
+      console.log(data.content);  // 如果 content 存在，则打印
+    } else {
+      console.log('没有 content 字段');
     }
+  }
+  
   },)
