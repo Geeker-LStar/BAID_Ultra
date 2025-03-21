336caf64f436f9746872713f98c6585067e3beb3
MRH0420 <1766634751@qq.com>
2025-03-21 13:28:07 +0800

    阶段性改一下UI。

--- miniprogram/pages/apps/apps/Lists/Test/subpage/subpage.js
+++ miniprogram/pages/apps/apps/Lists/Test/subpage/subpage.js
@@ -1,66 +1,19 @@
-// pages/apps/apps/Lists/Test/subpage/subpage.js
 Page({
-
-  /**
-   * Page initial data
-   */
-  data: {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage() {
-
-  }
-})
+    data: {
+        value1: '',
+        value2: '',
+        value3: '',
+        value4: '输入框已禁用',
+        value5: '',
+        value6: '',
+        value7: '',
+        switch : false
+    },
+    onChange(event){
+        const detail = event.detail;
+        this.setData({
+            'switch1' : detail.value
+        })
+        
+    }
+});
